50768e8e14d496bc8486dc8798002f486691cee8
Douglas Hennrich <douglas_b_h@hotmail.com>
2020-07-30 16:18:13 -0300

    Atualizando paleta de cores

--- Movies/Utilities/Colors.swift
+++ Movies/Utilities/Colors.swift
@@ -13,8 +13,8 @@ extension UIColor {
     static let background: UIColor = UIColor(red: 43/255.0, green: 45/255.0, blue: 66/255.0, alpha: 1.0)
     static let primary: UIColor = UIColor(red: 237/255.0, green: 242/255.0, blue: 244/255.0, alpha: 1.0)
     static let secondary: UIColor = UIColor(red: 141/255.0, green: 153/255.0, blue: 62/255.0, alpha: 1.0)
-    static let hightlight: UIColor = UIColor(red: 239/255.0, green: 35/255.0, blue: 60/255.0, alpha: 1.0)
-    static let hightlightOver: UIColor = UIColor(red: 217/255.0, green: 4/255.0, blue: 41/255.0, alpha: 1.0)
+    static let hightlight: UIColor = UIColor(red: 4/255.0, green: 102/255.0, blue: 60/255.0, alpha: 1.0)
+    static let hightlightOver: UIColor = UIColor(red: 2/255.0, green: 62/255.0, blue: 125/255.0, alpha: 1.0)
 
     // Used in Letter Image Generate
     struct ImageLetter {
